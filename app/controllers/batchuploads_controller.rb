c4e2a17884290a8fdc79d60bf13f5096eccee1aa
Sam Hopkins <s.a.hopkins@gmail.com>
2021-01-15 00:22:42 -0500

    record validation

--- app/controllers/batchuploads_controller.rb
+++ app/controllers/batchuploads_controller.rb
@@ -9,64 +9,23 @@ class BatchuploadsController < ApplicationController
   end
 
   def upload
-    id = params['identifier']
-    identifier = Identifier.find_by(identifier: id)
-    if identifier.nil?
-      identifier = Identifier.create(identifier: id)
-    end
+    identifier = Identifier.find_or_create_by(identifier: params['identifier'])
     identifier.record_errors.delete_all
     identifier.records.delete_all
-    tempfile = params['csvfile'].tempfile
-    csv = CSV.new(tempfile, headers: true)
+    csv = CSV.new(params['csvfile'].tempfile, headers: true)
     csv.map(&:to_h).each_with_index { |row, index|
-      errors = phone_errors(row['phone'])
-      errors += email_errors(row['email'])
-      errors += name_errors(row['first'], row['last'])
-      if errors.length != 0
-        errors.each { |err|
-          identifier.record_errors.create(row: index, text: err)
-        }
-      else
-        identifier.records.create(row: index, **row)
-      end
+      rec = identifier.records.create(row: index, **row)
+      errors = {}
+      rec.errors.each { |err|
+        errors[err.attribute] ||= "#{err.attribute.to_s.capitalize} #{err.message}"
+      }
+      errors.each_value { |e|
+        identifier.record_errors.create(row: index, text: e)
+      }
     }
-    #debugger
-    tempfile.close!()
     redirect_to output_url
   end
 
   private
 
-    def fmt_error(label, text)
-      return "#{label} field error: #{text}"
-    end
-
-    def phone_errors(phone)
-      errors = []
-      # required
-      bad_char_off = /[^0-9\-\.()]/ =~ phone
-      if bad_char_off != nil
-        errors << fmt_error("phone", "invalid character at position [#{bad_char_off}]")
-      end
-      digits = phone.gsub(/[^0-9]/, '')
-      if digits.length != 10
-        errors << fmt_error("phone", "number must contain exactly 10 digits")
-      end
-      if /[01]/ =~ (digits[0]+digits[3])
-        errors << fmt_error("phone", "number may not have 0 or 1 in the [0] or [3] position")
-      end
-      return errors
-    end
-
-    def email_errors(email)
-      # required
-      # must be standard and valid
-      return []
-    end
-
-    def name_errors(first, last)
-      # first and last must be alpha only and at least 2 characters long
-      # neither are required, but if first name is empty, last cannot be specified
-      return []
-    end
 end
--- app/models/record.rb
+++ app/models/record.rb
@@ -1,3 +1,39 @@
+
 class Record < ApplicationRecord
 	belongs_to :identifier
+	validates :row, presence: true
+	validates :phone,
+		presence: true,
+		format: { with: /\A[0-9\-\.()]+\z/,
+							message: "format error, only 0-9, -, ., (, ) permitted" }
+	validate :phone_digit_check
+	validates :email,
+		presence: true,
+		length: { maximum: 255 },
+		format: { with: /\A[\w+\-.]+@([a-z\d\-]+\.)+[a-z]{2,3}\z/i }
+	validate :first_last_dependent_presence
+	validates :first, :last,
+		allow_blank: true,
+		length: { minimum: 2, maximum: 255 },
+		format: { with: /\A[a-z]+\z/i,
+							message: "only alpha characters permitted" }
+
+	private
+
+		def phone_digit_check
+			return if phone.blank?
+			digits = phone.gsub(/[^0-9]/, '')
+			if digits.length != 10
+				errors.add(:phone, "must contain exactly 10 digits")
+			end
+			if /[01]/ =~ (digits[0]+digits[3])
+				errors.add(:phone, "may not have 0 or 1 in the [0] or [3] digit position")
+			end
+		end
+
+		def first_last_dependent_presence
+			if first.blank? && !last.blank?
+				errors.add(:first, "must be specified if last name is specified")
+			end
+		end
 end
