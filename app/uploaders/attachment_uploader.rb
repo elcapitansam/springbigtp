84e798ffafeb58ef957fda60bbb05dc1c729a4b9
Sam Hopkins <s.a.hopkins@gmail.com>
2021-01-19 08:33:42 -0500

    revert

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -1,7 +1,7 @@
-class AttachmentUploader < Carrierwave::Uploader::Base
+class AttachmentUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
-  # include Carrierwave::RMagick
-  # include Carrierwave::MiniMagick
+  # include CarrierWave::RMagick
+  # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
